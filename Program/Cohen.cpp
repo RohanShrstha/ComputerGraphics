a20d1cb5e62b35e086418ec80f41013e173840e8
MrBaka <rohanshrstha@gmail.com>
2025-01-30 21:25:08 +0545

    Update Cohen.cpp

--- Program/Cohen.cpp
+++ Program/Cohen.cpp
@@ -128,7 +128,6 @@ void init_clip() {
     line_clip(p1, p2, win_min, win_max);
 }
 
-
 void init_graph(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
